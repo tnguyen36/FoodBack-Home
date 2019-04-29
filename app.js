48f35df7566f99401b7cf14c6cc628c4acca6f41
Thi Nguyen <tsokye346@gmail.com>
2019-04-28 19:13:28 -0700

    configure app and initial home route

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// Home Route
+app.get("/", function(req, res) {
+	res.render("home");
+});
+
+
+
+app.listen(3000, function() {
+	console.log("Server has started!");
+});
